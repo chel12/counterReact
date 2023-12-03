8f1a803ed293804b38be93a9c4a6e4bf9e94babb
Семён Субочев <s.subochev@sz.ld>
2023-12-03 19:07:12 +0300

    app:[counterSucces]

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+	let [player1Counter, setPlayer1Counter] = useState(0);
+	let [player2Counter, setPlayer2Counter] = useState(0);
 	return (
 		<div>
 			<div>
 				<div>Иван Иванович</div>
-				<div>3</div>
-				<button>+</button>
+				<div>{player1Counter}</div>
+				<button
+					onClick={() => {
+						setPlayer1Counter(player1Counter + 1);
+					}}>
+					+
+				</button>
 			</div>
 			<hr />
 			<div>
 				<div>Пётр Петрович</div>
-				<div>3</div>
-				<button>+</button>
+				<div>{player2Counter}</div>
+				<button
+					onClick={() => {
+						setPlayer2Counter(player2Counter + 1);
+					}}>
+					+
+				</button>
 			</div>
 			<hr />
-			<button>-</button>
+			<button
+				onClick={() => {
+					setPlayer1Counter(player1Counter - 1);
+					setPlayer2Counter(player2Counter - 1);
+				}}>
+				-
+			</button>
 		</div>
 	);
 }
